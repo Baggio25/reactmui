8d2e356b4ca143f03866ffe4ae2697625daeeeaf
Rodrigo Baggio <rodrigo.baggio.si@gmail.com>
2024-09-11 23:10:46 -0300

    #32 - Consulta de registro no backend com React

--- src/pages/pessoas/DetalheDePessoas.tsx
+++ src/pages/pessoas/DetalheDePessoas.tsx
@@ -1,12 +1,37 @@
+/* eslint-disable no-restricted-globals */
+
 import { useNavigate, useParams } from "react-router-dom";
 
 import { LayoutBaseDePagina } from "../../shared/layouts";
 import { FerramentasDeDetalhe } from "../../shared/components";
+import { useEffect, useState } from "react";
+import { PessoasService } from "../../shared/services/api/pessoas/PessoasService";
+import { LinearProgress } from "@mui/material";
 
-export const DetalheDePessoas : React.FC = () => {
-  const { pessoaId = 'nova'} = useParams<'pessoaId'>();
-
+export const DetalheDePessoas: React.FC = () => {
+  const { pessoaId = 'nova' } = useParams<'pessoaId'>();
   const navigate = useNavigate();
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [nome, setNome] = useState('');
+
+  useEffect(() => {
+    if (pessoaId !== 'nova') {
+      setIsLoading(true);
+
+      PessoasService.getById(Number(pessoaId))
+        .then((result) => {
+          setIsLoading(false);
+
+          if (result instanceof Error) {
+            alert(result.message);
+            navigate('/pessoas');
+          } else {
+            setNome(result.nomeCompleto);
+          }
+        });
+    }
+  }, [pessoaId]);
 
   const handleNovoClick = () => {
     navigate("/pessoas/detalhe/nova");
@@ -20,28 +45,41 @@ export const DetalheDePessoas : React.FC = () => {
     console.log('save');
   }
 
-  const handleDeleteClick = () => {
-    console.log('delete');
+  const handleDeleteClick = (pessoaId: number) => {
+    if (confirm("Confirma a exclusão?")) {
+      PessoasService.deleteById(Number(pessoaId)).then((result) => {
+        if (result instanceof Error) {
+          alert(result.message);
+        } else {
+          navigate("/pessoas");
+          alert("Registro excluído com sucesso.");
+        }
+      });
+    }
   }
 
-  return(
+  return (
     <LayoutBaseDePagina
-      titulo="Detalhe de pessoa"
+      titulo={(pessoaId === 'nova' ? 'Nova pessoa' : nome)}
       barraDeFerramentas={
-        <FerramentasDeDetalhe 
+        <FerramentasDeDetalhe
           textoBotaoNovo="Nova"
           mostrarBotaoApagar={pessoaId !== 'nova'}
           mostrarBotaoNovo={pessoaId !== 'nova'}
           mostrarBotaoSalvarEFechar
 
           aoClicarEmSalvar={handleSaveClick}
-          aoClicarEmSalvarEFechar={() => {}}
+          aoClicarEmSalvarEFechar={() => { }}
           aoClicarEmVoltar={handleVoltarClick}
-          aoClicarEmApagar={handleDeleteClick}
+          aoClicarEmApagar={() => handleDeleteClick(Number(pessoaId))}
           aoClicarEmNovo={handleNovoClick}
         />
       }
     >
+
+      {isLoading && (
+        <LinearProgress variant="indeterminate" />
+      )}
 
       <p>Pessoa {pessoaId}</p>
     </LayoutBaseDePagina>
